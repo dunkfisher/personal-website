c3572667773e0b58d3b084352f7d3695c0b1edb3
Duncan Fisher <dunc1981@gmail.com>
2015-11-18 23:10:26 +0000

    Style bundle, tidy up files etc

---
+++ Website/Website/App_Start/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace Website.App_Start
+{
+    public class Test : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+    }
+}
