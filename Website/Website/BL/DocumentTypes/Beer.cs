9d02e798bf4281bb145ff543e283cc232903ab10
Duncan Fisher <dunc1981@gmail.com>
2015-12-01 23:43:02 +0000

    Work on beer listing page.

---
+++ Website/Website/BL/DocumentTypes/Beer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Website.Extensions;
+
+namespace Website.UI.BL.DocumentTypes
+{
+    public class Beer : Base
+    {
+        public string FullName { get { return Content.GetProperty<string>("fullName"); } }
+        public string Brewer { get { return Content.GetProperty<string>("brewer"); } }
+        public string CountryOfOrigin { get { return Content.GetProperty<string>("countryOfOrigin"); } }
+        public string Review { get { return Content.GetProperty<string>("review"); } }
+        public int Rating { get { return Content.GetProperty<int>("rating"); } }
+
+        public Beer(IPublishedContent content) : base(content)
+        {
+        }
+
+        public string GetImageUrl(UmbracoHelper umbraco)
+        {            
+            var mediaId = Content.GetProperty<int>("image");
+            return umbraco.TypedMedia(mediaId).Url;
+        }
+    }
+}
--- Website/Website/BL/DocumentTypes/Listing.cs
+++ Website/Website/BL/DocumentTypes/Listing.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Umbraco.Core.Models;
+using Umbraco.Web;
 
 namespace Website.UI.BL.DocumentTypes
 {
     public class Listing : Page
     {
+        public IEnumerable<Beer> Beers
+        {
+            get
+            {
+                var beers = new List<Beer>();
+                foreach (var beer in Content.Descendants<Beer>())
+                {
+                    beers.Add(beer);
+                }
+                return beers;
+            }
+        }
+
         public Listing(IPublishedContent content) : base(content)
         {
-        }    
+        }
     }
 }
